5813c1959475f37e426e928bae25efb12715760a
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2023-03-19 20:45:34 -0400

    Revert "not working"
    This reverts commit 70c7f1a306e6b512e620d04b87ecec3e1d91aac1.

--- Assets/Post_process.cs
+++ Assets/Post_process.cs
@@ -8,12 +8,7 @@ public class Post_process: MonoBehaviour
     public ComputeShader textureShader;
 
     private RenderTexture _rTexture;
-    public Light sun;    
-    public int NumSamples;
-    public int Density;
-    public int Weight;
-    public float Decay;
-    public int Exposure;
+    public Light sun;
     Camera cam;
     private void OnRenderImage(RenderTexture source, RenderTexture destintion){
         
@@ -55,12 +50,7 @@ public class Post_process: MonoBehaviour
 
     void Start()
     {
-        cam = GetComponent<Camera>();
-        NumSamples = 50;
-        Density = 1;
-        Weight = 1;
-        Decay = 0.25f;
-        Exposure = 1;
+      cam = GetComponent<Camera>();
     }
 
 }
